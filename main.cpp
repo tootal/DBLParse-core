3e3dc3ec6186c373c80787840d597686b85f9cbe
tootal <tootal@yeah.net>
2021-08-08 21:13:46 +0800

    feat: add cxxopts and vscode launch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <cxxopts.hpp>
 
-int main() {
-    std::cout << "hello world!\n";
+int main(int argc, char *argv[]) {
+    cxxopts::Options options("DBLParse-core", "The core of DBLParse");
+    options.allow_unrecognised_options();
+    options.add_options()("h,help", "Show help");
+
+    auto result = options.parse(argc, argv);
+    if (result["help"].as<bool>()) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    std::cout << options.help() << std::endl;
     return 0;
 }
